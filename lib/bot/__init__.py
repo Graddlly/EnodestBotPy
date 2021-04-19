4a1879f83fa4b76e80765cacf236d30360161c19
Graddlly <34636667+Graddlly@users.noreply.github.com>
2021-04-19 22:31:19 +0500

    [ANY] Инициализация сторонних функций (cogs)

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -3,25 +3,52 @@ from discord.ext.commands import CommandNotFound
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+from glob import glob
+
 from ..db import db
 
 PREFIX = '>'
 OWNER_IDS = []
+COGS = [path.split('\\')[-1][:-3] for path in glob('./lib/cogs/*.py')]
+
+class BotReady(object):
+    def __init__(self):
+        for cog in COGS:
+            setattr(self, cog, False)
+
+    def ready_up(self, cog):
+        setattr(self, cog, True)
+        print(f"[EnodestBot] {cog} cog ready")
+
+    def all_ready(self):
+        return all(getattr(self, cog) for cog in COGS)
 
 class BotS(BotBase):
     def __init__(self):
         super().__init__(command_prefix = PREFIX, owner_ids = OWNER_IDS)
 
+        self.PREFIX = PREFIX
         self.ready = False
         self.guild = None
+        self.cogs_ready = BotReady()
         self.scheduler = AsyncIOScheduler()
 
         db.autosave(self.scheduler)
 
         super().__init__(command_prefix = PREFIX, owner_ids = OWNER_IDS)
 
+    def setup(self):
+        for cog in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
+            print(f"[EnodestBot] {cog} cog loaded")
+
+        print('[EnodestBot] Setup: initializing cogs complete')
+
     def run(self, version):
         self.VERSION = version
+
+        print('[EnodestBot] Setup: running')
+        self.setup()
 
         with open('./lib/bot/token', mode = 'r', encoding = 'utf-8') as tokenfile:
             self.TOKEN = tokenfile.read()
@@ -48,8 +75,12 @@ class BotS(BotBase):
 
     async def on_ready(self):
         if not self.ready:
-            self.ready = True
             self.scheduler.start()
+            self.ready = True
+
+            while not self.cogs_ready.all_ready():
+                await sleep(0.5)
+
             print('[EnodestBot] Ready')
         else:
             print('[EnodestBot] Reconnecting...')
